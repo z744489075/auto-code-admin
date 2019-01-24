37b6b9b64a0d45cb6b63c960a8c73f3882810775
zengtengpeng <zengtengpeng@etiaolong.com>
2019-01-24 10:02:10 +0800

    权限问题

--- auto-code-common/src/main/java/com/zengtengpeng/common/AutoCodeTest.java
+++ auto-code-common/src/main/java/com/zengtengpeng/common/AutoCodeTest.java
@@ -8,14 +8,14 @@ import java.util.List;
 
 public class AutoCodeTest {
     public static void main(String[] args) {
-        List<String> dataNames= Arrays.asList("sys_operation_log","sys_login_log");
+        List<String> dataNames= Arrays.asList("test_code");
         StartCode startCode=new StartCode();
         startCode.setJdbc("jdbc:mysql://localhost:3306/auto_code");
         startCode.setUser("root");
         startCode.setPassword("111111");
         startCode.setDataNames(dataNames);
-        startCode.setParentPath("E:\\resource\\workspaceJDB\\auto-code-admin\\auto-code-sys");
-        startCode.setParentPack("com.zengtengpeng.sys");
+        startCode.setParentPath("E:\\resource\\workspaceJDB\\auto-code-admin\\auto-code-web");
+        startCode.setParentPack("com.zengtengpeng.test");
         AutoCodeUtils.startByWebAdmin(startCode);
     }
 }
